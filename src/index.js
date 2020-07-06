655bb97a04f7b40b61b88051c998d7d8723d39af
ryan <ryannadiputraa@gmail.com>
2020-07-06 18:38:32 +0800

    added webpack config

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './style/index.scss';
+import { ok } from './js/script-1';
+
+console.log(ok);
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,42 @@
+const htmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-
-  entry: './src/js/script1.js',
-
-  output: {
-    path: 'dist',
-    filename: 'index.js'
-  },
-
   module: {
-    loaders: [
-      {},
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      },
     ]
-  }
-
+  },
+  plugins: [
+    new htmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+  ]
 }
